e77ee754dca560cc82c38cdbe39cd930917f27f3
Santiago <santiago.garciah17@gmail.com>
2025-02-19 18:18:31 -0500

    feat: Test for Text Input

---
+++ src/test/java/com/testingplayground/TextInputTest.java
@@ -0,0 +1,37 @@
+package com.testingplayground;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class TextInputTest extends BaseTest {
+
+    @Test
+    public void testTextInput() {
+        WebElement btnTextInput = driver.findElement(
+                By.xpath("//a[text()='Text Input']")
+        );
+        btnTextInput.click();
+
+        String message = "Hola mundo";
+
+        WebElement inputText = driver.findElement(
+                By.xpath("//input[@id='newButtonName']")
+        );
+        inputText.sendKeys(message);
+
+        WebElement btnChangeValue = driver.findElement(
+                By.xpath("//button[@id='updatingButton']")
+        );
+        btnChangeValue.click();
+
+        Assertions.assertEquals(message, btnChangeValue.getText());
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
