108d19a07e9ac61f82e3006d18dda0c9d5c05b70
Santiago <santiago.garciah17@gmail.com>
2025-02-19 18:14:34 -0500

    feat: Test for Overlapped Element

---
+++ src/test/java/com/testingplayground/OverlappedElementTest.java
@@ -0,0 +1,36 @@
+package com.testingplayground;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+public class OverlappedElementTest extends BaseTest {
+
+    @Test
+    public void testOverlappedElement() {
+        WebElement btnOverlappedElement = driver.findElement(
+                By.xpath("//a[text()='Overlapped Element']")
+        );
+        btnOverlappedElement.click();
+
+        WebElement inputId = driver.findElement(
+                By.xpath("//input[@id='id']")
+        );
+        inputId.sendKeys("12");
+
+        String name = "Juan";
+
+        WebElement inputName = driver.findElement(
+                By.xpath("//input[@id='name']")
+        );
+
+        JavascriptExecutor jsExecitor = (JavascriptExecutor) driver;
+        jsExecitor.executeScript("arguments[0].scrollIntoView();", inputName);
+
+        inputName.sendKeys(name);
+
+        Assertions.assertEquals(name, inputName.getAttribute("value"));
+    }
+}
