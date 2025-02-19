733dc8ebb642796e98834ab3e4a5e7577abef63e
Santiago <santiago.garciah17@gmail.com>
2025-02-19 18:18:08 -0500

    feat: Test for Shadow DOM

---
+++ src/test/java/com/testingplayground/ShadowDomTest.java
@@ -0,0 +1,44 @@
+package com.testingplayground;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
+
+public class ShadowDomTest extends BaseTest {
+
+    @Test
+    public void testShadowDom() {
+        WebElement btnShadowDom = driver.findElement(
+                By.xpath("//a[text()='Shadow DOM']")
+        );
+        btnShadowDom.click();
+
+        WebElement shadowHost = driver.findElement(
+                By.tagName("guid-generator")
+        );
+
+        SearchContext shadowRoot = shadowHost.getShadowRoot();
+
+        WebElement btnGenerate = shadowRoot.findElement(
+                By.id("buttonGenerate")
+        );
+        btnGenerate.click();
+
+        WebElement btnCopy = shadowRoot.findElement(
+                By.id("buttonCopy")
+        );
+        btnCopy.click();
+
+        WebElement inputGUID = shadowRoot.findElement(
+                By.id("editField")
+        );
+
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        String clipboardText = (String) jsExecutor.executeScript("return navigator.clipboard.readText();");
+
+        Assertions.assertEquals(inputGUID.getAttribute("value"), clipboardText);
+    }
+}
