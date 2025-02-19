d7e57509169cc1c4ae6af05b68d0530dcf892933
Santiago <santiago.garciah17@gmail.com>
2025-02-19 18:15:29 -0500

    feat: Test for Progress Bar

---
+++ src/test/java/com/testingplayground/ProgressBarTest.java
@@ -0,0 +1,45 @@
+package com.testingplayground;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ProgressBarTest extends BaseTest {
+
+    @Test
+    public void testProgressBarTest() {
+        WebElement btnProgressBar = driver.findElement(
+                By.xpath("//a[text()='Progress Bar']")
+        );
+        btnProgressBar.click();
+
+        WebElement btnStart = driver.findElement(
+                By.xpath("//button[@id='startButton']")
+        );
+        btnStart.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        String progress = "75";
+
+        WebElement lblProgressBar = wait.until(
+                ExpectedConditions.presenceOfElementLocated(
+                        By.xpath(
+                                String.format("//div[@id='progressBar' and @aria-valuenow='%s']", progress)
+                                )
+                )
+        );
+
+        WebElement btnStop = driver.findElement(
+                By.xpath("//button[@id='stopButton']")
+        );
+        btnStop.click();
+
+        Assertions.assertTrue(lblProgressBar.getText().contains(progress));
+    }
+}
